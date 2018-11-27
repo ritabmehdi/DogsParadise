18b0a263837b73dfcef9d2bf5af7c2d544489c41
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2018-11-27 12:12:06 +0100

    WIP - to be finished after lunch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,9 +6,17 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @dog = Dog.find(params[:dog_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @booking.dog = Dog.find(params[:dog_id])
+    @booking.total_price = @booking.dog.fee * (@booking.end_date - @booking.start_dates)
+    @booking.user = current_user
+    @booking.save
   end
 
   def edit
@@ -18,5 +26,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end

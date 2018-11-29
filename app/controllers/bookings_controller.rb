0caa9db2c2a99abea1dd03f084e5232213cb7f21
Mehdi Ritab <ritabmehdi@gmail.com>
2018-11-29 16:51:06 +0100

    Mehdi master changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -65,4 +65,5 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
   # has_one :user, through: :dog
+
+  def bookin_ref
+    return self.id.to_s + "B" + self.dog.id.to_s + "D" + self.created_at.strftime("%Y%m%d")
+  end
 end

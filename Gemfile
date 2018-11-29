eb4e7a4e3277bd2c2c6d196b82d4fb89c14c876c
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2018-11-29 16:36:39 +0100

    Booking logic implemented but still missing either error messages or calendar logic

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'validates_overlap'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dog
+
+  validates :start_date, :end_date, :overlap => {:scope => "user_id", :message_title => "Some validation title", :message_content => "Some validation message"}
   # has_one :user, through: :dog
 end
